a5919120dd262764f862b4bb929db58360b5a034
Oren Weinberg <Oren.Weinberg@cellebrite.com>
2021-03-03 13:35:10 +0200

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,9 @@
 console.log('Client file');
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data);
-    })
-})
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
-// messageOne.textContent = 'ssssssss'
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -23,7 +15,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'loading forcast...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if(data.error){
                 messageOne.textContent = data.error;    
