539908c7c3ec27296b4f97ba712103a5b0f683e3
Oren Weinberg <Oren.Weinberg@cellebrite.com>
2021-03-08 10:05:40 +0200

    v1

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,11 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
+const messageThree = document.querySelector('#message-3')
+
+const messageOne1 = document.querySelector('#message-11')
+const messageTwo1 = document.querySelector('#message-22')
+const messageThree1 = document.querySelector('#message-33')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -17,11 +22,20 @@ weatherForm.addEventListener('submit', (e) => {
 
     fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
+            console.log('======================================================');
+            console.log(data);
+            console.log('======================================================');
             if(data.error){
                 messageOne.textContent = data.error;    
             } else {
-                messageOne.textContent = data.location;    
-                messageTwo.textContent = JSON.stringify(data.forecast);                 
+                console.log(JSON.stringify(data));
+                messageOne.textContent = JSON.stringify(data.country[0]) + ' ' +  JSON.stringify(data.itemId[0]);    
+                messageTwo.textContent = JSON.stringify(data.title[0]);                 
+                messageThree.textContent = JSON.stringify(data.price[0]);                 
+
+                messageOne1.textContent = JSON.stringify(data.country[1]) + ' ' +  JSON.stringify(data.itemId[1]);    
+                messageTwo1.textContent = JSON.stringify(data.title[1]);                 
+                messageThree1.textContent = JSON.stringify(data.price[1]);                 
             }        
         })
     })
--- src/app.js
+++ src/app.js
@@ -12,6 +12,26 @@ const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
+let eBay = require('ebay-node-api')
+
+let ebayUK = new eBay({
+    clientID: 'OrenWein-comparep-SBX-7dc78fcfb-22b79c89',
+    env: 'SANDBOX', // optional default = 'PRODUCTION',
+    headers:{ // optional
+        'X-EBAY-C-MARKETPLACE-ID': 'EBAY_GB' // For Great Britain https://www.ebay.co.uk
+    },
+    'GLOBAL-ID':'EBAY_GB'
+    
+})
+
+let ebaySP = new eBay({
+    clientID: 'OrenWein-comparep-SBX-7dc78fcfb-22b79c89',
+    env: 'SANDBOX', // optional default = 'PRODUCTION',
+    headers:{ // optional
+        'X-EBAY-SOA-GLOBAL-ID': 'EBAY_ES' // For Great Britain https://www.ebay.co.uk
+    },
+    'GLOBAL-ID':'EBAY_SP'
+})
 
 // Setup handlesbars engine and views location
 app.set('view engine', 'hbs')
@@ -48,25 +68,92 @@ app.get('/weather', (req, res) => {
         return res.send({
             error:'You must a insert an address'
         })
-    }
+    }    
 
-    geocode(req.query.address, (error, {lat, lot, location} = {}) => {
-        if (error) {
-            return res.send({ error })            
-        }
+    ebayUK.findItemsByKeywords({
+        keywords: req.query.address,
+        sortOrder: 'PricePlusShippingLowest', //https://developer.ebay.com/devzone/finding/callref/extra/fndcmpltditms.rqst.srtordr.html
+        pageNumber: 1,
+        limit: 3
         
-        forecast(lat, lot, (error, forecastData) => {
-            if(error) {
-                return res.send({ error })            
-            }        
+    }).then((data) => {        
+        let items = []
+        // console.log(JSON.stringify(data[0].ack[0]));
+        // console.log(JSON.stringify(data));
+
+        let item = data[0].searchResult[0].item[0];
+        let price  = item.sellingStatus[0].currentPrice[0]
+
+        items[0] = {
+            itemId: item.itemId,
+            country: item.country,
+            title: item.title,                
+            price: price['__value__'] + ' : ' + price['@currencyId']
+        }
+
+        console.log('---------------------------------------');
+        console.log(items[0]);
+
+        ebaySP.findItemsByKeywords({
+            keywords: req.query.address,
+            sortOrder: 'PricePlusShippingLowest', //https://developer.ebay.com/devzone/finding/callref/extra/fndcmpltditms.rqst.srtordr.html
+            pageNumber: 1,
+            limit: 3
+            
+        }).then((data) => {        
+            // console.log(JSON.stringify(data));
+            console.log(JSON.stringify(data[0].ack[0]));
+    
+            let item1 = data[0].searchResult[0].item[0];
+            let price1  = item.sellingStatus[0].currentPrice[0]
+    
+            items[1] = {
+                itemId: item1.itemId,
+                country: item1.country,
+                title: item1.title,                
+                price: price1['__value__'] + ' : ' + price1['@currencyId']
+            }
+
+            console.log('---------------------------------------');
+            console.log(items[1]);
     
             res.send({
-                forecast: forecastData,
-                location,                
-                address: req.query.address
+                itemId: [items[0].itemId, items[1].itemId],
+                country: [items[0].country, items[1].country],
+                title: [items[0].title, items[1].title],                
+                price: [items[0].price, items[1].price],                
             })
-        })
-    })
+        }, (error) => {
+            console.log(error);
+        });
+
+        // res.send({
+        //     itemId: item.itemId,
+        //     country: item.country,
+        //     title: item.title,                
+        //     price: price['__value__'] + ' : ' + price['@currencyId']
+        // })
+    }, (error) => {
+        console.log(error);
+    });
+
+    // geocode(req.query.address, (error, {lat, lot, location} = {}) => {
+    //     if (error) {
+    //         return res.send({ error })            
+    //     }
+        
+    //     forecast(lat, lot, (error, forecastData) => {
+    //         if(error) {
+    //             return res.send({ error })            
+    //         }        
+    
+    //         res.send({
+    //             forecast: forecastData,
+    //             location,                
+    //             address: req.query.address
+    //         })
+    //     })
+    // })
 
     // res.send({
     //     location:req.query.address,
